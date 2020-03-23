f19c33d6d178f3dbc5b5df48e5df73953b4d1780
faroeq-linux-new <alves_1044@hotmail.com>
2020-03-23 12:47:53 +0100

    fixed indentation in classes

--- src/JsonConversion.php
+++ src/JsonConversion.php
@@ -3,25 +3,25 @@ namespace IMDB;
 
 class JsonConversion
 {
-	private $input;
-	private $url;
-	private $json;
-	private $data;
+    private $input;
+    private $url;
+    private $json;
+    private $data;
 
-	public function __construct($url, $input) {
-		$this->url = $url;
-		$this->input = $input;
-	}
+    public function __construct($url, $input) {
+        $this->url = $url;
+        $this->input = $input;
+    }
 
-	public function convertToPHParray( ){
+    public function convertToPHParray( ) {
         $jsonFileContents = file_get_contents( $this->url . $this->input );
         $phpFileContents = json_decode($jsonFileContents, true);// json to php array conversion
 
         $this->json = $jsonFileContents;
         $this->data = $phpFileContents;
-		}
+    }
 
-	public function getData(){
+    public function getData() {
         return $this->data;
     }
 }
