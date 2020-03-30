219382b07aeb5627c56a7607812f0df319672c72
faroeq-linux-new <alves_1044@hotmail.com>
2020-03-30 18:42:24 +0200

    further changes

--- cruds/search.php
+++ cruds/search.php
@@ -1,10 +1,9 @@
 <?php
-require __DIR__.'/vendor/autoload.php';
-require_once 'loader.php';
+require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ .'loader.php';
 
 use IMDB\Movie as Movie;
 use IMDB\Helper as Helper;
-
 
 if ( isset( $_POST['title'] ) )
 {
--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 require_once 'vendor/autoload.php';
 require_once 'loader.php';
 
+// vardump base directory constant
+
 echo $twig->render('core.html.twig');
