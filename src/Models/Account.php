460bc6c3bdf35439e2e4bbbb99849b27954f23ae
Faroeq <alves_1044@hotmail.com>
2021-06-17 21:32:14 +0200

    fix create user

--- src/Models/Account.php
+++ src/Models/Account.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace IMDB\Models;
 
 
@@ -11,140 +12,124 @@ use PDOException;
  * Class Account
  * @package IMDB\Models
  */
-class Account
-{
-    public $accountid;
-    public $username;
-    private $password;
+class Account {
+	public $accountid;
+	public $username;
+	private $password;
 
-    private $error;
+	private $error;
 
-    /**
-     * Account constructor.
-     * @param $username
-     * @param $password
-     */
-    public function __construct($username, $password)
-    {
-        $hashedPassword = Password ::hash($password);
+	/**
+	 * Account constructor.
+	 * @param $username
+	 * @param $password
+	 */
+	public function __construct($username, $password) {
+		$hashedPassword = Password::hash($password);
 
-        $this->username = $username;
-        $this->password = $hashedPassword;
-    }
+		$this->username = $username;
+		$this->password = $hashedPassword;
+	}
 
-    /**
-     *
-     */
-    public function register()
-    {
-        try {
-            $database = new Database();
+	/**
+	 *
+	 */
+	public function register() {
+		try {
+			$database = new Database();
 
-            $sql = "INSERT INTO account ( username, password ) VALUES ( :username, :password )";
-            $database->query($sql);
-            $database->bind(":username", $this->username);
-            $database->bind(":password", $this->password);
-            $database->resultSet();
-        } catch (PDOException $e) {
+			$sql = "INSERT INTO account ( username, `password` ) VALUES ( :username, :password )";
+			$database->query($sql);
+			$database->bind(":username", $this->username);
+			$database->bind(":password", $this->password);
+			$database->resultSet();
+		} catch (PDOException $e) {
 
-            echo "error";
-            echo 'Connection failed: ' . $e->getMessage();
-            echo $this->error;
-        }
-    }
+			echo "error";
+			echo 'Connection failed: ' . $e->getMessage();
+			echo $this->error;
+		}
+	}
 
 
-    public function setAccountId()
-    {
-        try {
-            $database = new Database();
+	public function setAccountId() {
+		try {
+			$database = new Database();
 
-            $sql = "SELECT id_account FROM account WHERE username = :username";
-            $database->query($sql);
-            $database->bind(":username", $this->username);
-            $fetchedAccountId = $database->resultSet();
+			$sql = "SELECT id_account FROM account WHERE username = :username";
+			$database->query($sql);
+			$database->bind(":username", $this->username);
+			$fetchedAccountId = $database->resultSet();
 
-            $this->accountid = $fetchedAccountId;
+			$this->accountid = $fetchedAccountId;
+		} catch (PDOException $e) {
 
-        } catch (PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+		}
+	}
 
-            echo 'Connection failed: ' . $e->getMessage();
-        }
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAccountid()
-    {
-        return $this->accountid;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getAccountid() {
+		return $this->accountid;
+	}
 
 
-    /**
-     * @param $username
-     * @return \Exception|PDOException
-     */
-    public function findUser( $username )
-    {
-        try
-        {
-            $database = new Database();
-            $sql = "SELECT username FROM account WHERE (username = :username)";
-            $database->query($sql);
-            $database->bind(":username", $username);
+	/**
+	 * @param $username
+	 * @return \Exception|PDOException
+	 */
+	public function findUser($username) {
+		try {
+			$database = new Database();
+			$sql = "SELECT username FROM account WHERE (username = :username)";
+			$database->query($sql);
+			$database->bind(":username", $username);
 
-            return $database->resultSet();
-        }
-        catch (PDOException $exception)
-        {
-            echo "oh nee!";
-            return $exception;
-        }
-    }
+			return $database->resultSet();
+		} catch (PDOException $exception) {
+			echo "oh nee!";
+			return $exception;
+		}
+	}
 
-    /**
-     * @param $username
-     * @param $password
-     * @return bool
-     */
-    public function verifyUser( $username, $password ) {
-        //fetching hashPassword from relevent username$
-        $hashedPassword = $this->getHashedPassword( $username );
+	/**
+	 * @param $username
+	 * @param $password
+	 * @return bool
+	 */
+	public function verifyUser($username, $password) {
+		//fetching hashPassword from relevent username$
+		$hashedPassword = $this->getHashedPassword($username)['password'];
 
-        return Password::verifyHash(
-            $password,
-            $hashedPassword['password']
-        );
-    }
+		$result = password_verify($password, $hashedPassword);
 
-    /**
-     * @param $username
-     * @return \Exception|PDOException
-     */
-    public function getHashedPassword($username )
-    {
-        try
-        {
-            $database = new Database();
-            $sql = "SELECT password FROM account WHERE ( username = :username )";
-            $database->query($sql);
-            $database->bind(":username", $username);
+		return $result;
+	}
 
-            return $database->resultSet();
-        }
-        catch (PDOException $exception)
-        {
-            echo "Geen rijen opgevangen" . "<br>";
-            return $exception;
-        }
-    }
+	/**
+	 * @param $username
+	 * @return \Exception|PDOException
+	 */
+	public function getHashedPassword($username) {
+		try {
+			$database = new Database();
+			$sql = "SELECT password FROM account WHERE ( username = :username )";
+			$database->query($sql);
+			$database->bind(":username", $username);
 
-    /**
-     * @return mixed
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
+			return $database->resultSet();
+		} catch (PDOException $exception) {
+			echo "Geen rijen opgevangen" . "<br>";
+			return $exception;
+		}
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getUsername() {
+		return $this->username;
+	}
 }
